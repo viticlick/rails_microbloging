9ae57226f5dc3b47af5e6461ceb689d9ac18cedc
Víctor López <victor@barkibu.com>
2022-07-12 09:06:47 +0200

    Destroy Foo controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
